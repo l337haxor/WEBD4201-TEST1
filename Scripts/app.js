d8271b5279aef24d30a96906a05242ec08feec7e
l337haxor <47485167+l337haxor@users.noreply.github.com>
2020-01-28 15:39:33 -0500

    Added button click functionality

--- Scripts/app.js
+++ Scripts/app.js
@@ -7,22 +7,20 @@
 "use strict";
 //IIFE - Immediately Invoked Function Expression
 // mean? -> anonymous self-executing function
-
 let app;
-//Reference for the large button
-let largeButton;
-//A h3 element
-let h3Student;
-//Reference to parent node of the large button
-let largeButtonParentNode;
-//Reference to the firstParagraph id
-let firstParagraphEdit;
-
 
 (function(app){
 
     // Declare Function Variables here...
     console.log("%cDeclaring Variables", "color: red;")
+    //Reference for the large button
+    let largeButton;
+    //A h3 element
+    let h3Student;
+    //Reference to parent node of the large button
+    let largeButtonParentNode;
+    //Reference to the firstParagraph id
+    let firstParagraphEdit;
 
     /**
      * Variable initialization in this function
@@ -49,9 +47,31 @@ let firstParagraphEdit;
         //Change the text
         firstParagraphEdit.textContent = "JavaScript is a prototype-based language, and every object in JavaScript has a hidden internal property called Prototype that can be used to extend object properties and methods.";
 
-        //
+        //Hide the element
         firstParagraphEdit.style.cssText = "display: none";
 
+        //Shows the first paragraph (on click) if the button says "Learn more", otherwise it hides the paragraph when the button says "Hide Details"
+        largeButton.addEventListener("click", function(){
+
+            if(largeButton.textContent == "Hide Details")
+            {
+                //Hide the element
+                firstParagraphEdit.style.cssText = "display: none";
+                //Change the text
+                largeButton.textContent = "Learn More";
+            }
+            else
+            {
+                //Hide the element
+                firstParagraphEdit.style.cssText = "display: all";
+                //Change the text
+                largeButton.textContent = "Hide Details";
+
+            }
+            
+
+
+        });
         Main();
     }
 
