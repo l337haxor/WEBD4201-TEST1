8e14755e61e55c7caab3276bf5155235b139e26d
l337haxor <47485167+l337haxor@users.noreply.github.com>
2020-01-28 15:20:27 -0500

    Changed large button text, added h3 element

--- Scripts/app.js
+++ Scripts/app.js
@@ -9,6 +9,13 @@
 // mean? -> anonymous self-executing function
 
 let app;
+//Reference for the large button
+let largeButton;
+//A h3 element
+let h3Student;
+//Reference to parent node of the large button
+let largeButtonParentNode;
+
 (function(app){
 
     // Declare Function Variables here...
@@ -20,7 +27,19 @@ let app;
      */
     function Start()
     {
+        //Reference to the large button
+        largeButton = document.getElementById("largeButton");
+        //Change the button text
+        largeButton.lastChild.textContent = "Learn More";
 
+        //Create an h3 element
+        h3Student = document.createElement("h3");
+        //Change the text content of the h3 element
+        h3Student.textContent = "Sterling Wenzelbach 100299329";
+        //Get the parent node of the large button
+        largeButtonParentNode = largeButton.parentNode;
+        //Insert the h3 element before the large button
+        largeButtonParentNode.insertBefore(h3Student, largeButton);
 
         Main();
     }
