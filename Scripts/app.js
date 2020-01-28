a77343b3445641cdfe4dbef78d6722dc2cb2d4c7
l337haxor <47485167+l337haxor@users.noreply.github.com>
2020-01-28 15:08:34 -0500

    real first commit

---
+++ Scripts/app.js
@@ -0,0 +1,41 @@
+/*
+    Student Name  : Sterling Wenzelbach
+    StudentID     : 100299329
+    Date Completed: 2020-01-28
+*/
+
+"use strict";
+//IIFE - Immediately Invoked Function Expression
+// mean? -> anonymous self-executing function
+
+let app;
+(function(app){
+
+    // Declare Function Variables here...
+    console.log("%cDeclaring Variables", "color: red;")
+
+    /**
+     * Variable initialization in this function
+     *
+     */
+    function Start()
+    {
+
+
+        Main();
+    }
+
+    /**
+     * Main Program entry point is here
+     *
+     */
+    function Main()
+    {
+
+    }
+
+
+
+    window.addEventListener("load", Start);
+})(app || (app = {}));
+
